a7a8b4cb8f3253459b093ce25a15380d2ebddff9
diegogarcia666@gmail.com <diegogarcia666@gmail.com@aedd406f-791c-97e3-c158-25da2ea538be>
2012-10-11 04:56:47 +0000

    archivos.h

---
+++ tp1/include/Estacionamiento.h
@@ -0,0 +1,29 @@
+#ifndef ESTACIONAMIENTO_H
+    #define ESTACIONAMIENTO_H
+    #include <array>
+    #include <map>
+
+    using namespace std;
+
+    class Estacionamiento
+    {
+        private:
+        int lugaresLibres;
+        map <int,lugar> lugares;
+
+        void inicializarMap(int lugares){
+            for(int i=0, i<lugares, i++)
+                this.lugaresLibres.add(i,LIBRE);
+        };
+
+        public:
+            Estacionamiento(int lugares, int tiempoSimulacion){
+                lugaresLibres = lugares;
+            };
+
+            Estacionamiento();
+            virtual ~Estacionamiento();
+        protected:
+    };
+
+#endif // ESTACIONAMIENTO_H
---
+++ tp1/include/constantes.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTES_H_INCLUDED
+#define CONSTANTES_H_INCLUDED
+
+    #define CAPACIDAD_ESTACIONAMIENTO   400
+    #define CANT_ENTRADAS               3
+    #define CANT_SALIDAS                2
+    #define TIEMPO_SIMULACION           8
+
+    enum estadoLugar{LIBRE,OCUPADO};
+
+#endif // CONSTANTES_H_INCLUDED
