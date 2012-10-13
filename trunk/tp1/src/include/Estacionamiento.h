cd0f2f98d84ef542b011e22ae5b701a24b522fc7
invernizzie@gmail.com <invernizzie@gmail.com@aedd406f-791c-97e3-c158-25da2ea538be>
2012-10-13 20:43:47 +0000

    Estacionamiento.h con miembros public

--- trunk/tp1/src/include/Estacionamiento.h
+++ trunk/tp1/src/include/Estacionamiento.h
@@ -1,31 +1,45 @@
 #ifndef ESTACIONAMIENTO_H
-    #define ESTACIONAMIENTO_H
-    #include <vector>
-    #include "lugar.h"
+#define ESTACIONAMIENTO_H
+#include <vector>
+#include "lugar.h"
 
-    using namespace std;
+using namespace std;
 
-    class Estacionamiento
-    {
-        private:
-        int lugaresLibres;
-        vector<estadoLugar> lugares;
+class Estacionamiento {
 
-        void inicializarMapa(int lugares){
-            for(int i = 0; i < lugares; i++)
-                this->lugares.push_back(LIBRE);
+    public:
+        Estacionamiento();
+        virtual ~Estacionamiento();
+        void liberar(int posicion);
+        void ocupar(int posicion);
+        int posicionesLibres();
+        void ocuparPosicion();
+
+};
+
+/* Clase Estacionamiento de Diego
+class Estacionamiento
+{
+    private:
+    int lugaresLibres;
+    vector<estadoLugar> lugares;
+
+    void inicializarMapa(int lugares){
+        for(int i = 0; i < lugares; i++)
+            this->lugares.push_back(LIBRE);
+    };
+
+    public:
+        Estacionamiento(int lugares, int tiempoSimulacion){
+            this->lugaresLibres = lugares;
+            this->lugares.reserve(lugares);
+            inicializarMapa(lugares);
         };
 
-        public:
-            Estacionamiento(int lugares, int tiempoSimulacion){
-                this->lugaresLibres = lugares;
-                this->lugares.reserve(lugares);
-                inicializarMapa(lugares);
-            };
-
-            Estacionamiento();
-            virtual ~Estacionamiento();
-        protected:
-    };
+        Estacionamiento();
+        virtual ~Estacionamiento();
+    protected:
+};
+*/
 
 #endif // ESTACIONAMIENTO_H
