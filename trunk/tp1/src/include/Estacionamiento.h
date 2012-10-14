f1beb12c5ec85875175a0fd421124c00d19f815b
pabloberrilio@gmail.com <pabloberrilio@gmail.com@aedd406f-791c-97e3-c158-25da2ea538be>
2012-10-14 19:15:29 +0000

    Agrego estacionamiento... con locks y shared memory

--- trunk/tp1/src/include/Estacionamiento.h
+++ trunk/tp1/src/include/Estacionamiento.h
@@ -5,16 +5,36 @@
 
 using namespace std;
 
+class EstacionamientoCompleto : public Exception(){ }
+
+class LugarOcupado : public Exception() { }
+
 class Estacionamiento {
 
+    enum Parking_Status { FREE, BUSY };
+
     public:
-        Estacionamiento();
+        Estacionamiento(int capacidad = CAPACIDAD_ESTACIONAMIENTO);
         virtual ~Estacionamiento();
+
+        // Para ser usados por el auto.
         void liberar(int posicion);
         void ocupar(int posicion);
-        int posicionesLibres();
-        void ocuparPosicion();
 
+        // Para ser usados por las entradas y las salidas.
+        void liberarLugar();
+        void ocuparLugar();
+
+private:
+
+    //std::vector< int* > parking_availability_posicions_;
+    std::vector< MemoriaCompartida<int> > shared_memories_; // para lierarlas cuando ya no se usen.
+    std::vector< LockFile > posicions_lockers_; //lock para cada posicion.
+
+    int occupied_posicions_;
+    LockFile ocupacion_lock_;
+
+    void create_posicion(int pos_num);
 };
 
 /* Clase Estacionamiento de Diego
