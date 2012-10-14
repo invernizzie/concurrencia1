8c259a0005b06f76032a3818313543414fe860c9
diegogarcia666@gmail.com <diegogarcia666@gmail.com@aedd406f-791c-97e3-c158-25da2ea538be>
2012-10-14 19:25:01 +0000

    acá están los archivos usados en la prueba hecha con ECLIPSE

---
+++ trunk/tp1/prueba1/src/prueba1.cpp
@@ -0,0 +1,53 @@
+//============================================================================
+// Name        : prueba1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <errno.h>
+
+using namespace std;
+
+int main() {
+	int cant_ent = 3;
+	pid_t p;
+	long salto = 0;
+	for (int i = 0; i< cant_ent; i++){
+		salto++;
+		p = fork();
+		if (!p){
+			break;
+		}
+	}
+	if (!p)
+	{
+		cout << "Soy un proceso hijo" << endl;
+
+		time_t start;
+		time (&start);
+		cout << "salto vale: " << salto << endl;
+		srand ( time(NULL) + salto);
+		int  espera = rand() % 10;
+		cout << "Voy a esperar " << espera << " segundos antes de generar un proceso" << endl;
+		while (difftime (time(NULL),start) < espera);
+
+		p = fork();
+		if (!p)
+		{
+			cout << "Soy un proceso nieto" << endl;
+			cout << "Espere: " << espera << " segundos" << endl;
+		}
+		else
+				cout << "Soy un proceso hijo de nuevo" << endl;
+	}
+	else
+		cout << "Soy un proceso padre" << endl;
+	return 0;
+
+}
