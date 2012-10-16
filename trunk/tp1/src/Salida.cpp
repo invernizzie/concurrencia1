fcd818449b2c0aff1296cc3fddbfabca6f56e1f7
diegogarcia666@gmail.com <diegogarcia666@gmail.com@aedd406f-791c-97e3-c158-25da2ea538be>
2012-10-16 06:28:59 +0000

    Agregado del valor de la facutacion, el valor horario, el registro de la facturacion y la posicion en la salida (esto es apra que la salida sea la que libera el lugar, pero podemos modificarlo

--- trunk/tp1/src/Salida.cpp
+++ trunk/tp1/src/Salida.cpp
@@ -11,16 +11,6 @@ void Salida::liberarEspacio(){
 
 }
 
-void Salida::registrarPago(){
-
-}
-
-void Salida::seccionCritica(){
-    //this.liberarEspacio();
-    //this.registrarPago();
-
-}
-
 void Salida::run(){
 
    while((difftime (time(NULL),inicio.leer()) < tiempoSimulacion) ||
@@ -30,20 +20,6 @@ void Salida::run(){
             // hacer wait() de la cola de autos_listos
             // sacar el primero
             // hacer el signal
-
-        // ejecutar el pago del ticket de ese auto
-            // hacer el wait del recurso compartido CAJA
-            // efectuar el pago, agregando CANT_HORAS_TICKET * VALOR_HORA al total de la caja
-            // haer el signal del recurso compartido CAJA
-            // marcar el ticket como PAGADO
-
-
-
-        // liberar el espacio que ocupaba ese auto
-            // hacer el wait de ese lugar
-            // cambiar el estado del lugar a liberado
-            // sumar 1 a espacios disponibles
-            // hacer el signal del lugar
 
             estacionamiento.liberar();
 
--- trunk/tp1/src/include/Salida.h
+++ trunk/tp1/src/include/Salida.h
@@ -20,17 +20,12 @@
             MemoriaCompartida<time_t> inicio;
             MemoriaCompartida<Estacionamiento> estacionamiento;
 
-            void seccionCritica();
-
             void run(); // Aca seteo la memoria compartida con los mapas de posiciones del estacionamiento
 
             void liberarEspacio(); // Aca seteo el espacio que libero cuando un auto deja marcada la posicion para salir
 
-            void registrarPago(); // Aca se registra el pago luego de la liberacion del espacio que ocupaba el auto que acabo de marcar como liberada su posicion
-            // unsigned tiempoEntreArribos();
-
         public:
-
+            unsigned posicion;
             Salida(string nombre,int tSimulacion);
             virtual ~Salida();
 
