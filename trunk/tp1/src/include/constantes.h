7d2b9dd0cf5081bfc5e707e824b8e13b5ff42422
invernizzie@gmail.com <invernizzie@gmail.com@aedd406f-791c-97e3-c158-25da2ea538be>
2012-10-17 06:05:51 +0000

    Eliminacion de constantes literales. Borro clase deprecada y en desuso.

--- trunk/tp1/src/include/constantes.h
+++ trunk/tp1/src/include/constantes.h
@@ -15,6 +15,13 @@
     #define ARCHIVO_LOCK_OCUPACION      "ocupacion_lock.tmp"
     #define ARCHIVO_LOCK_FACTURACION    "facturacion_lock.tmp"
 
+    #define C_SHM_CAPACIDAD             'C'
+    #define C_SHM_PRECIO                'P'
+    #define C_SHM_TIEMPO_INICIO         't'
+    #define C_SHM_POS_OCUPADAS          'p'
+    #define C_SHM_VALOR_FACTURADO       'F'
+    #define C_SHM_LUGAR_ESTACIONAMIENTO 'l'
+
     enum EstadoLugar { LIBRE, OCUPADO, PARA_LIBERAR };
 
 #endif // CONSTANTES_H_INCLUDED
