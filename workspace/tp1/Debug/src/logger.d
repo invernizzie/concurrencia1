e591d27bf1bc11c274348ae3d20cb1aa85dae43e
invernizzie@gmail.com <invernizzie@gmail.com@aedd406f-791c-97e3-c158-25da2ea538be>
2012-11-25 21:13:33 +0000

    Quito directorio de build del repositorio porque molesta

--- workspace/tp1/Debug/src/logger.d
+++
@@ -1,8 +0,0 @@
-src/logger.d: ../src/logger.cpp ../src/include/logger.h \
- ../src/include/constantes.h ../src/include/LockFile.h
-
-../src/include/logger.h:
-
-../src/include/constantes.h:
-
-../src/include/LockFile.h:
