8800c6b9f0556fb64d9559b7ab20eeea8fc1f8d4
diegogarcia666@gmail.com <diegogarcia666@gmail.com@aedd406f-791c-97e3-c158-25da2ea538be>
2012-12-04 22:36:16 +0000

    Alineamiento y ajuste. El archivo MemoriaCompartida.h estaba mal porque tenía los includes separados por TAB. Se cambiaron a espacios.

--- trunk/workspace/tp2/src/include/Mensajes.h
+++ trunk/workspace/tp2/src/include/Mensajes.h
@@ -1,57 +1,57 @@
 #ifndef MENSAJES_H_
-#define MENSAJES_H_
+	#define MENSAJES_H_
 
-#include <sys/types.h>
+	#include <sys/types.h>
 
-// Tipos de pedido
-// Auto paga y desocupa posicion
-#define P_PAGO_DESOCUPO		1
-// Auto solicita una posicion vacia
-#define P_PIDO_LUGAR		2
-// Entrada intenta reservar un lugar
-#define P_OCUPO_LUGAR		3
-// Administrador consulta estado de su estacionamiento
-#define P_CONSULTA_ESTADO	4
-// Salida hace que Auto se retira
-#define P_SE_VA				5
-// Salida hace que se libere un lugar especificado
-#define P_LIBERA_LUGAR		6
-// Se utiliza para dar aviso de fin de simulacion
-#define P_FINALIZAR		7
-// Entrada termina su ejecucion
-#define P_TERMINO_ENTRADA			8
-// Administrador termina su ejecucion
-#define P_TERMINO_ADMINISTRADOR		9
+	// Tipos de pedido
+	// Auto paga y desocupa posicion
+	#define P_PAGO_DESOCUPO		1
+	// Auto solicita una posicion vacia
+	#define P_PIDO_LUGAR		2
+	// Entrada intenta reservar un lugar
+	#define P_OCUPO_LUGAR		3
+	// Administrador consulta estado de su estacionamiento
+	#define P_CONSULTA_ESTADO	4
+	// Salida hace que Auto se retira
+	#define P_SE_VA				5
+	// Salida hace que se libere un lugar especificado
+	#define P_LIBERA_LUGAR		6
+	// Se utiliza para dar aviso de fin de simulacion
+	#define P_FINALIZAR		7
+	// Entrada termina su ejecucion
+	#define P_TERMINO_ENTRADA			8
+	// Administrador termina su ejecucion
+	#define P_TERMINO_ADMINISTRADOR		9
 
-typedef struct Pedido {
-	long mtype;
-	pid_t pid;
-	int nroEstacionamiento;
+	typedef struct Pedido {
+		long mtype;
+		pid_t pid;
+		int nroEstacionamiento;
 
-	// Solo para que el auto pague
-	unsigned nroLugar;
-	unsigned duracionEstadia;
-} Pedido;
+		// Solo para que el auto pague
+		unsigned nroLugar;
+		unsigned duracionEstadia;
+	} Pedido;
 
-typedef struct EstadoEstacionamiento {
-	int facturacion;
-	int lugaresLibres;
-} EstadoEstacionamiento;
+	typedef struct EstadoEstacionamiento {
+		int facturacion;
+		int lugaresLibres;
+	} EstadoEstacionamiento;
 
-typedef struct Respuesta {
-	long mtype;
-	union Resultado {
-		// Respuesta a ocupacion de lugar (Entrada)
-		bool habiaLugar;
-		// Respuesta a pedido de lugar (Auto)
-		unsigned lugarOtorgado;
-		// Respuesta a consulta de estado (Adm)
-		EstadoEstacionamiento estado;
-		// Repuesta a liberar (Salida)
-		bool lugarLiberado;
-		// Repuesta a pagar (Auto)
-		bool pagoAceptado;
-	} respuesta;
-} Respuesta;
+	typedef struct Respuesta {
+		long mtype;
+		union Resultado {
+			// Respuesta a ocupacion de lugar (Entrada)
+			bool habiaLugar;
+			// Respuesta a pedido de lugar (Auto)
+			unsigned lugarOtorgado;
+			// Respuesta a consulta de estado (Adm)
+			EstadoEstacionamiento estado;
+			// Repuesta a liberar (Salida)
+			bool lugarLiberado;
+			// Repuesta a pagar (Auto)
+			bool pagoAceptado;
+		} respuesta;
+	} Respuesta;
 
 #endif /* MENSAJES_H_ */
--- trunk/workspace/tp2/src/include/Proceso.h
+++ trunk/workspace/tp2/src/include/Proceso.h
@@ -1,22 +1,22 @@
 #ifndef PROCESO_H_
-#define PROCESO_H_
+	#define PROCESO_H_
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <errno.h>
-#include "constantes.h"
-#include "LockFile.h"
-#include "MemoriaCompartida.h"
+	#include <unistd.h>
+	#include <stdlib.h>
+	#include <errno.h>
+	#include "constantes.h"
+	#include "LockFile.h"
+	#include "MemoriaCompartida.h"
 
-class Proceso {
-    public:
-        Proceso() : pid(0) {};
-        virtual ~Proceso() {};
-        pid_t iniciar();
-        virtual void ejecutar() {};
+	class Proceso {
+		public:
+			Proceso() : pid(0) {};
+			virtual ~Proceso() {};
+			pid_t iniciar();
+			virtual void ejecutar() {};
 
-    protected:
-        pid_t pid;
-};
+		protected:
+			pid_t pid;
+	};
 
 #endif /* PROCESO_H_ */
